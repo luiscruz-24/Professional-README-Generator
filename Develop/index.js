318a5fb68e877904e3df9f22ede516c431cda7df
Luis Cruz <luis.crz.v@gmail.com>
2021-05-15 16:09:07 -0500

    package-lock.json, the questions and the const with the packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project',
+        name: 'description',
+    },
+    {
+        default: 'npm i',
+        message: 'The command to begin the installation',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter the usage information for your project',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter the contribution guidelines',
+        name: 'contribution',
+    },
+    {
+        type: 'list',
+        message: 'Select one licence for your project',
+        name: 'license',
+        choices: ['Apache 2.0 License', 'MIT License', 'GPL License', 'Mozilla Public License 2.0', 'ISC License'],
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
